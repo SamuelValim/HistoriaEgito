cae37452dc65065beb2243cf720788f9f4696822
SamuelValim <s.v.n.valim@gmail.com>
2023-10-14 17:45:14 -0300

    Update Proj Rev.5

---
+++ js/script_menu.js
@@ -0,0 +1,19 @@
+// Script responsável pela automação do menu hamburger
+
+const btnMobile = document.getElementById('btn-mobile');
+// const >> declara a nossa variável, no uso do decorre do codigo
+
+function toggleMenu(event) {
+    if (event,type == 'touchstart') event,preventDefault (); const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute('arie-expanded',active);
+    if (active){
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+    } else {
+        event.currentTarget.setAttribute('aria-label','Abrir Menu')
+    }
+}
+
+btnMobile.addEventListener('click',toggleMenu)
+btnMobile.addEventListener('touchstart',toggleMenu)
